f4bd672ebe3f381963300914434cdc0d40a610c6
dbga_38 <dbga_38@dbga.local>
2016-09-14 17:21:00 +0200

    Screen Cursor

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    SkinnedMeshRenderer colorEnemy;
+
+    void Start ()
+    {
+        HiglightedPower();
+	}
+	
+
+	void Update ()
+    {
+	
+	}
+
+    public void HiglightedPower()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+    public Image cursorImage;
+
+	void Start ()
+    {
+
+	}
+	
+
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Mouse1))
+            cursorImage.gameObject.SetActive(true);
+
+        else
+            cursorImage.gameObject.SetActive(false);
+    }
+}
