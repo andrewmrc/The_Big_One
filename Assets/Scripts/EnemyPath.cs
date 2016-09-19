03176fb3cee9eb1d2aa44fcc7e183f6b2be4c075
dbga_37 <dbga_37@dbga.local>
2016-09-19 18:15:57 +0200

    navmesh prova

---
+++ Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPath : MonoBehaviour {
+
+    NavMeshAgent refNav;
+    
+
+	// Use this for initialization
+	void Start () {
+        refNav = GetComponent<NavMeshAgent>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        refNav.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        Animator animPlayer = GetComponent<Animator>();
+        animPlayer.SetFloat("Forward", 0.5f);
+        
+    }
+}
