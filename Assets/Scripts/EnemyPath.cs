fd8d343153ff640fc8f73f42258ac150cb917f72
dbga_37 <dbga_37@dbga.local>
2016-09-22 18:05:00 +0200

    testing power

--- Assets/Scripts/EnemyPath.cs
+++ Assets/Scripts/EnemyPath.cs
@@ -6,52 +6,60 @@ public class EnemyPath : MonoBehaviour {
     NavMeshAgent refNav;
     GameManager refGM;
     public Transform position_one;
-    public Vector3 position_zero;
+    Vector3 initialPosition;
     private float timeLeft;
+    public Transform[] points = new Transform[4];
+    bool ciao;
+    
+    public int input;
 
     // Use this for initialization
     void Awake () {
         
         refNav = GetComponent<NavMeshAgent>();
         refGM = FindObjectOfType<GameManager>();
-        position_zero = this.transform.position;
+        initialPosition = this.transform.position;
+        
     }
 
     void OnEnable()
     {
         timeLeft = 3.0f;
-        refNav.destination = position_one.position;
+        GoToPoint(input);
+        ciao = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
         
-        //if (GameManager.Self.UI_Power.active == true)
+        if (refNav.remainingDistance < 0.5f)
         {
-            print(refNav.pathEndPosition);
-            Animator animPlayer = GetComponent<Animator>();
-            animPlayer.SetFloat("Forward", 0.6f);           
+            if (!ciao)
+            {
+                timeLeft = Time.time;
+                ciao = true;
+            }
             
-            if (refNav.remainingDistance < 0.01f)
+            if (Time.time - timeLeft > 3)
             {
-                timeLeft -= Time.deltaTime;                
-                animPlayer.SetFloat("Forward", 0);
-               
-            }
-            if (timeLeft < 0)
-            {
-                refNav.destination = position_zero;
-                animPlayer.SetFloat("Forward", 0.6f);
-                if (refNav.remainingDistance < 0.01f && timeLeft < -0.1f)
-                {
-                    animPlayer.SetFloat("Forward", 0);
-                    this.GetComponent<EnemyPath>().enabled = false;
-                    
-                }
-            }
+                ResetPosition();
+            }            
         }
+    }
 
-        
-        
+    void ResetPosition()
+    {
+        refNav.destination = initialPosition;
+        if (refNav.remainingDistance < 0.5f)
+        {
+            GetComponent<EnemyPath>().enabled = false;
+        }
+    }
+
+    void GoToPoint(int _input)
+    {
+
+        refNav.destination = points[input].position;
+
     }
 }
