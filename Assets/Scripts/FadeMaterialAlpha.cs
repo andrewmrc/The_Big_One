f80b637b01dfe5a59de8bca16591230e2da3ee8e
Andrea Mercuri <andrea.mercuri21@gmail.com>
2016-10-12 02:02:33 +0200

    - Inseriti 3D Text descrittivi per oggetti e persone. - Inserito shader per i 3D Text per impedire di vedersi attraverso qualsiasi cosa. - Creati script per permettere ai 3D Text di ruotare seguendo la posizione della camera. "RotateFollow" - Creato script che permette ai 3D Text di fare fade in fade out in base ad una distanza del player minima e massima pubbliche. "FadeMaterialAlpha" - Inserito un font stile "Journal" per provare. - Inserita un 3DText all'attaccapanni e alla receptionist per provare. - Le prove sono salvate nella scena TestAngelo.

---
+++ Assets/Scripts/FadeMaterialAlpha.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeMaterialAlpha : MonoBehaviour {
+
+	public float minRange = 1.5f;
+	public float maxRange = 3f;
+
+	void Update () {
+
+		// calcoliamo l'alpha in base alla distanza e ai range settati
+		float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+		float lerpAmt = 1.0f - Mathf.Clamp01((distance - minRange) / (maxRange - minRange));
+
+		// aggiorniamo l'alpha del materiale
+		Renderer renderer = transform.GetComponent<Renderer>();
+		Color color = renderer.material.color;
+		color.a = lerpAmt;
+		renderer.material.color = color;
+
+		Debug.Log(string.Format("Distance = {0} \t(range = {1} to {2})\t LerpAmt = {3}", distance, minRange, maxRange, lerpAmt));
+	}
+}
---
+++ Assets/Scripts/RotateFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateFollow : MonoBehaviour {
+
+	//public int rotSpeed = 3;
+
+	//private GameObject target;
+	private Vector3 targetPoint;
+	private Quaternion targetRotation;
+
+	void  Start (){	
+		//target = GameObject.FindWithTag("MainCamera");
+	}
+	
+	void  Update (){
+		transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+	}
+
+}
