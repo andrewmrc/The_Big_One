806cbdfd7f19917626e36245335058593ad223c3
David Fiorin <david94lozoo@gmail.com>
2016-10-26 08:07:12 +0200

    movimento (Work in progress)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ public class Movement : MonoBehaviour {
 
     Animator playerAnim;
     float mouseX;
+    bool isMoving;
 
 	void Start ()
     {
@@ -15,20 +16,40 @@ public class Movement : MonoBehaviour {
 	void Update ()
     {
         mouseX = Input.GetAxis("Mouse X");
-        transform.Rotate( Vector3.up, mouseX * 100f * Time.deltaTime, Space.World);
+
+
+        if (isMoving)
+        {
+            transform.Rotate(Vector3.up, mouseX * 100f * Time.deltaTime, Space.World);
+            //transform.forward = Camera.main.transform.forward;
+        }
+        else
+        {
+            transform.Rotate(Vector3.up * 0f);
+        }
 
 
         if (Input.GetKey(KeyCode.W))
         {
             playerAnim.SetFloat("Forward", 1f);
+            isMoving = true;
         }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            playerAnim.SetFloat("Forward", 0.5f);
-        }
+
         else
         {
             playerAnim.SetFloat("Forward", 0.5f);
+            isMoving = false;
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+            this.transform.Rotate(transform.up, -100 * Time.deltaTime, Space.World);
+
+        if (Input.GetKey(KeyCode.D))
+            this.transform.Rotate(transform.up, 100 * Time.deltaTime, Space.World);
+
+        if (Input.GetKey(KeyCode.S))
+        {
         }
     }
 }
