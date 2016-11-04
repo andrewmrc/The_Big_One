9631227cad74f3f5ec3c1c1c3908b8b8a0d5d56d
dbga_37 <dbga_37@dbga.local>
2016-11-04 18:19:13 +0100

    GameFlow Temporaneo in sviluppo

---
+++ Assets/Scripts/ProvaFlow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEditor;
+
+public enum Condition {nothing, isTrigger, isConversation, quellochevuoi }
+
+public class ProvaFlow : MonoBehaviour {
+    public UnityEvent newEvent;
+    public string normalConversation;
+
+    public bool isTrigger = false;
+    //public bool isConversation = false;
+    public Condition wichCondition = Condition.isTrigger;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter()
+    {
+        if (wichCondition == Condition.isTrigger)
+        {
+            newEvent.Invoke();
+        }
+    }
+
+    void OnTriggerStay()
+    {
+        if (wichCondition == Condition.isConversation)
+        {
+            Debug.LogWarning(normalConversation);
+          
+        }
+    }
+
+    
+}
