b72de997944ae8f40bc22bfeb0bd3123fb9a4c24
dbga_38 <dbga_38@dbga.local>
2016-11-10 18:03:12 +0100

    continuo rail camera

--- Assets/Scripts/RailCamera.cs
+++ Assets/Scripts/RailCamera.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class RailCamera : MonoBehaviour {
 
     public byte i = 0;
+    public Transform initialPos;
 
     public Transform[] cubeArray = new Transform[5];
     //public Transform olivia;
@@ -11,8 +12,7 @@ public class RailCamera : MonoBehaviour {
     void Start ()
     {
         StartCoroutine(railCamera());
-
-
+        initialPos = this.transform;
     }
 
     void Update ()
@@ -23,25 +23,60 @@ public class RailCamera : MonoBehaviour {
     private IEnumerator railCamera()
     {
         Vector3 currentPos = this.transform.position;
+        float distance = 1;
 
         while (true)
         {
-            this.transform.position = Vector3.Lerp(this.transform.position, cubeArray[i].position, Time.deltaTime * 1.5f);
+
+            //this.transform.LookAt(oliviaTarget);
+            this.transform.position = Vector3.Lerp(this.transform.position, cubeArray[i].position, Time.deltaTime * 3f);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, cubeArray[i].transform.rotation, Time.deltaTime * 2);
+
+
             if (Input.GetKeyDown(KeyCode.E))
             {
                 yield break;
             }
 
-            if ((currentPos - cubeArray[i].position).magnitude > 2f)
+            if ((this.transform.position - cubeArray[i].position).magnitude > distance)
             {
                 yield return null;
             }
+
             else
             {
                 i++;
-                currentPos = this.transform.position;
+                //currentPos = this.transform.position;
+                distance = 0.5f;
+
                 yield return null;
+
+                /*if (i == cubeArray.Length-1)
+                {
+                    Debug.Log("i'm in");
+                    this.transform.position = Vector3.Lerp(this.transform.position, cubeArray[i].position, Time.deltaTime * 4f);
+
+                    if ((this.transform.position - cubeArray[i].position).magnitude > 0.01f)
+                    {
+                        yield return null;
+                    }
+                    //i++;
+                }*/
+
+                if (i >= 5)
+                {
+                    distance = 0.01f;
+                    Debug.Log(distance);
+                    this.transform.position = initialPos.position;
+                    this.transform.rotation = Quaternion.Slerp(this.transform.rotation, initialPos.rotation, Time.deltaTime * 2);
+                    //this.gameObject.SetActive(false);
+                    //yield return new WaitForSeconds(1f);
+                    yield break;
+                }
             }
-        }
+       }
+        this.transform.position = initialPos.position;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, initialPos.rotation, Time.deltaTime * 2);
+
     }
 }
