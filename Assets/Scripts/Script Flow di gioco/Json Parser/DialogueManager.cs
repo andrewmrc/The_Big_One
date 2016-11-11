233458d637675fcbdae331fe2ca4d002a98601fb
dbga_71 <dbga_71@dbga.local>
2016-11-11 14:20:39 +0100

    Nuove prove dialoghi

--- Assets/Scripts/Script Flow di gioco/Json Parser/DialogueManager.cs
+++ Assets/Scripts/Script Flow di gioco/Json Parser/DialogueManager.cs
@@ -31,7 +31,7 @@ public class DialogueManager : MonoBehaviour
     void Start()
     {
 
-        cv.parseJSON("ProvaAngelo.json");
+        cv.parseJSON("DialogoNuovo.json");
         actualNode = cv.getDialogue(moveThroughNodes);
         //cv.debugDialogues();
         //setting
