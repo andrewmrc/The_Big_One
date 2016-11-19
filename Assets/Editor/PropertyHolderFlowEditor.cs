e3b54bd94614635015eb79e4996ddfccf76fd123
fushja <fabrizio.caggianelli@gmail.com>
2016-11-19 20:37:50 +0100

    Messo apposto la visulizzazione del flow manager

--- Assets/Editor/PropertyHolderFlowEditor.cs
+++ Assets/Editor/PropertyHolderFlowEditor.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 [CustomEditor(typeof(FlowManager)), CanEditMultipleObjects]
 public class PropertyHolderFlowEditor : Editor
 {
-
+    bool showPosition = true;
+    string status = "Select a GameObject";
     //FlowManager t;
     SerializedObject GetTarget;
     public SerializedProperty
         randomArray_Prop,
         sequenceArray_Prop,
         asd;
-         
-        //returnEvent_Prop,
-        
+
+    //returnEvent_Prop,
+
 
     void OnEnable()
     {
         // Setup the SerializedProperties
         //t = (FlowManager)target;
-        
+
         randomArray_Prop = serializedObject.FindProperty("flowRandomGameArray");
         sequenceArray_Prop = serializedObject.FindProperty("flowGameArray");
         asd = serializedObject.FindProperty("asd");
@@ -33,14 +34,12 @@ public class PropertyHolderFlowEditor : Editor
     {
         serializedObject.Update();
         Show(serializedObject.FindProperty("flowRandomGameArray"));
-        
         Show(serializedObject.FindProperty("flowGameArray"));
-
         serializedObject.ApplyModifiedProperties();
 
     }
 
-    void Show (SerializedProperty list)
+    void Show(SerializedProperty list)
     {
         EditorGUILayout.BeginHorizontal();
         EditorGUILayout.PropertyField(list);
@@ -50,50 +49,42 @@ public class PropertyHolderFlowEditor : Editor
 
         if (list.isExpanded)
         {
-            
+
             for (int i = 0; i < list.arraySize; i++)
             {
-                
-                EditorGUILayout.PropertyField(list.GetArrayElementAtIndex(i));
+
                 SerializedProperty MyListRef = list.GetArrayElementAtIndex(i);
-
-                //if (MyListRef.isExpanded)
                 
+                
+                EditorGUILayout.PropertyField(MyListRef);
+                if (MyListRef.isExpanded)
+                {                    
+                    
 
 
-                    SerializedProperty sequenceName = MyListRef.FindPropertyRelative("SequenceName");
                     SerializedProperty callAction = MyListRef.FindPropertyRelative("call");
                     SerializedProperty arrayBool = MyListRef.FindPropertyRelative("sequence");
-
-
-
-                    EditorGUILayout.PropertyField(sequenceName);
-                
+                    SerializedProperty sequenceName = MyListRef.FindPropertyRelative("SequenceName");
+                    status = sequenceName.stringValue;
+                    sequenceName.stringValue = EditorGUILayout.TextField("Nome ", sequenceName.stringValue);
                     EditorGUILayout.PropertyField(callAction);
                     EditorGUILayout.BeginHorizontal();
-                    //EditorGUILayout.PropertyField(arrayBool,GUIContent.none);
                     arrayBool.arraySize = EditorGUILayout.IntSlider("N sequenze ", arrayBool.arraySize, 1, 10);
-                    //EditorGUILayout.PropertyField(arrayBool.FindPropertyRelative("Array.size"));
                     EditorGUILayout.EndHorizontal();
-
                     EditorGUILayout.BeginHorizontal();
                     for (int a = 0; a < arrayBool.arraySize; a++)
                     {
-
                         arrayBool.GetArrayElementAtIndex(a).boolValue = EditorGUILayout.Toggle(arrayBool.GetArrayElementAtIndex(a).boolValue);
-
-
+                        
                     }
-                
-
-                EditorGUILayout.EndHorizontal();
-
-
+                    
+                    EditorGUILayout.EndHorizontal();
+                }
             }
-            
+
         }
         EditorGUI.indentLevel -= 1;
-        
+
 
     }
 }
