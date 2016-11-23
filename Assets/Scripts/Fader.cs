0ea99abd1bb9e1d2c87f76a6f81e66dcc232abc9
dbga_38 <dbga_38@dbga.local>
2016-11-23 15:14:16 +0100

    Rail Camera

---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fader : MonoBehaviour {
+
+    public GameObject fade;
+
+    public IEnumerator FadeIn()
+    {
+        while (fade.GetComponent<CanvasGroup>().alpha < 1)
+        {
+            fade.GetComponent<CanvasGroup>().alpha += 1f * Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeOut()
+    {
+        while (fade.GetComponent<CanvasGroup>().alpha > 0)
+        {
+            fade.GetComponent<CanvasGroup>().alpha -= 1f * Time.deltaTime;
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/RailCamera.cs
+++ Assets/Scripts/RailCamera.cs
@@ -1,63 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class RailCamera : MonoBehaviour {
+namespace UnityStandardAssets.Cameras {
 
-    private byte i = 0;
-    public float distance = 1f;
-    public float waitTime = 1f;
-    public float speed = 3f;
-    public GameObject fade;
-    public List<Transform> cubeList = new List<Transform>(5);
-    public List<Transform> listTarget = new List<Transform>();
+    public class RailCamera : MonoBehaviour {
 
-    void Start ()
-    {
-        StartCoroutine(railCamera());
-    }
+        private byte i = 0;
+        public float distance = 1f;
+        public float waitTime = 1f;
+        public float speed;
+        public GameObject mainCamera;
+        public List<Transform> cubeList = new List<Transform>(5);
+        public List<Transform> listTarget = new List<Transform>();
+        Fader refFader;
 
-    private IEnumerator railCamera()
-    {
-        Vector3 currentPos = this.transform.position;
-
-        while (true)
+        void Start()
         {
-            this.transform.position = Vector3.Lerp(this.transform.position, cubeList[i].position, Time.deltaTime * speed);
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, cubeList[i].transform.rotation, Time.deltaTime * speed);
+           
+            refFader = FindObjectOfType<Fader>();
+            StartCoroutine(railCamera());
+        }
 
-            if (Input.GetKeyDown(KeyCode.E))
+        private IEnumerator railCamera()
+        {
+            mainCamera.GetComponent<FreeLookCam>().enabled = false;
+            Vector3 currentPos = this.transform.position;
+
+            while (true)
             {
-                yield break;
-            }
+                this.transform.position = Vector3.Lerp(this.transform.position, cubeList[i].position, speed * 0.05f);
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, cubeList[i].transform.rotation, speed * 0.05f);
 
-            if ((this.transform.position - cubeList[i].position).magnitude > distance)
-            {
-                //this.transform.LookAt(listTarget[i]);
-                yield return null;
-            }
-            else
-            {
-                yield return new WaitForSeconds(waitTime);
-                i++;
-                distance = 0.5f;
-
-                yield return null;
-
-                if (i >= 5)
+                if (Input.GetKeyDown(KeyCode.E))
                 {
-                    FadeIn();
-                    this.gameObject.SetActive(false);
-                    
-                    yield return new WaitForSeconds(3);
                     yield break;
                 }
-            }
-       }
-    }
 
-    public void FadeIn()
-    {
-        fade.GetComponent<CanvasGroup>().alpha += 0.5f;
+                // Se la distanza tra la camera ed i cubi è MAGGIORE della distance continua a muoverti
+                if ((this.transform.position - cubeList[i].position).magnitude >= distance)
+                {
+                    //this.transform.LookAt(listTarget[i]);
+                    yield return null;
+                }
+
+                // Se la distanza tra la camera ed i cubi è MINORE della distance aspetta waitTime
+                // punta al nuovo target (i++) 
+                else
+                {
+                    yield return new WaitForSeconds(waitTime);
+                    i++;
+
+                    // Opzionale... Quando devo fare l'ultima transizione abbasso il tempo di attesa a 0
+                    if (i == cubeList.Count -1)
+                        waitTime = 0f;
+
+                    if (i >= cubeList.Count)
+                    {
+                        
+                        Debug.Log("Dovrebbe iniziare ORA");
+                        // Esegue il FadeIn in Fader
+                        refFader.StartCoroutine(refFader.FadeIn());
+                        yield return new WaitForSeconds(2);
+
+                        // Esegue il FadeOut in Fader e riattiva la MainCamera
+                        refFader.StartCoroutine(refFader.FadeOut());
+                        mainCamera.GetComponent<FreeLookCam>().enabled = true;
+
+                        // Infine spengo la RailCamera
+                        this.gameObject.SetActive(false);
+                        yield break;
+                    }
+                }
+            }
+        }     
     }
 }
