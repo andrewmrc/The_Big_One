09823ee327790936af03f19c014db63904624b71
fushja <fabrizio.caggianelli@gmail.com>
2016-11-28 22:01:40 +0100

    doorHandler modificato: Ora la variabile door rotation indica di quanto si vuole ruotare la porta

--- Assets/Scripts/DoorHandler.cs
+++ Assets/Scripts/DoorHandler.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class DoorHandler : MonoBehaviour {
 
 	public float doorRotation;
-
+    private Vector3 defaultRot;
+    private Vector3 openRot;
+    
 	// Use this for initialization
 	void Start () {
-	
-	}
+        defaultRot = transform.eulerAngles;
+        openRot = new Vector3(defaultRot.x, defaultRot.y + doorRotation, defaultRot.z);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,7 +20,8 @@ public class DoorHandler : MonoBehaviour {
 
 	void OnCollisionEnter (Collision coll) {
 		if(coll.gameObject.tag == "Player") {
-			Debug.Log("APRI PORTA");
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, 1);
+            /*Debug.Log("APRI PORTA");
 
 			ContactPoint contact = coll.contacts[0];
 			Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
@@ -26,9 +30,11 @@ public class DoorHandler : MonoBehaviour {
 			Debug.Log (coll.contacts [0].point);
 			Debug.Log ("ROT: " + rot);
 			//this.gameObject.transform.rotation = Quaternion.Euler (this.gameObject.transform.rotation.x - 90f, this.gameObject.transform.rotation.y, this.gameObject.transform.rotation.z);
-			this.gameObject.transform.rotation = rot;
+			this.gameObject.transform.rotation = rot;*/
 
-		}
+
+
+        }
 	}
 
 }
