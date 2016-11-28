7bc79038fbb731ee23c19c48b80754a8c8415db5
dbga_04 <dbga_04@dbga.local>
2016-11-28 18:27:41 +0100

    - Aggiornato modello Hospital - Iniziato script porte

---
+++ Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorHandler : MonoBehaviour {
+
+	public float doorRotation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision coll) {
+		if(coll.gameObject.tag == "Player") {
+			Debug.Log("APRI PORTA");
+
+			ContactPoint contact = coll.contacts[0];
+			Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+			Vector3 pos = contact.point;
+
+			Debug.Log (coll.contacts [0].point);
+			Debug.Log ("ROT: " + rot);
+			//this.gameObject.transform.rotation = Quaternion.Euler (this.gameObject.transform.rotation.x - 90f, this.gameObject.transform.rotation.y, this.gameObject.transform.rotation.z);
+			this.gameObject.transform.rotation = rot;
+
+		}
+	}
+
+}
