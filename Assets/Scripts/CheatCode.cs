402d8f95e9b09fe3f4d939c88fbef928da39c155
Andrea Mercuri <andrea.mercuri21@gmail.com>
2016-11-29 02:11:20 +0100

    - Aggiornata scena ospedale con nuovi elementi, dialoghi, npc. - Creato script con i cheat code per caricare ogni scena con un tasto o ricaricare la scena stessa in caso di bisogno.

---
+++ Assets/Scripts/CheatCode.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CheatCode : MonoBehaviour {
+
+	Fader refFader;
+
+	public void Start () {
+		refFader = FindObjectOfType<Fader>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P)){
+			refFader.StartCoroutine(refFader.FadeIn());
+			StartCoroutine (LoadScene ("Level_Office"));
+		} else if(Input.GetKeyDown(KeyCode.L)) {
+			refFader.StartCoroutine(refFader.FadeIn());
+			StartCoroutine (LoadScene ("Level_Hospital"));
+		} else if(Input.GetKeyDown(KeyCode.I)) {
+			refFader.StartCoroutine(refFader.FadeIn());
+			StartCoroutine (LoadScene ("Main_Menu"));
+		} else if(Input.GetKeyDown(KeyCode.O)) {
+			refFader.StartCoroutine(refFader.FadeIn());
+			StartCoroutine (LoadScene (SceneManager.GetActiveScene().name));
+		}
+	}
+
+
+	IEnumerator LoadScene (string levelName) {
+		yield return new WaitForSeconds (1.5f);
+		SceneManager.LoadScene (levelName);
+	}
+}
