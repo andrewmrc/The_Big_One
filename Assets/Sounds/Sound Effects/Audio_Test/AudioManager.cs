662dbc4f85ac98fb12cde19ac4e5a968be956b27
Riccardo Torregrossa <Riccardo Torregrossa>
2016-12-25 04:26:08 +0100

    Creata scena test per i suoni. Creato mixer e script per l'assegnazione dei suoni.

---
+++ Assets/Sounds/Sound Effects/Audio_Test/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(UnityEngine.AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+	public AudioClip otherClip;
+	public Animator OliviaAnimator;
+	private bool Walk;
+
+	void Start () 
+	{
+		Walk = false;
+	}
+
+	void Update () 
+	{
+		AudioSource audio = GetComponent<AudioSource> ();
+
+		if (OliviaAnimator.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+			{
+				Walk = true;
+				audio.clip = otherClip;
+				audio.Play();
+			}
+
+
+		if (OliviaAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+			{
+					Walk = false;
+					audio.clip = otherClip;
+					audio.Stop();
+			}
+	}
+}
