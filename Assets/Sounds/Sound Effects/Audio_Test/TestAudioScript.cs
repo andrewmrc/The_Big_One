cd9a06c9209d5c384b42bb4a0adabbaaaa94bd10
dbga_14 <dbga_14@dbga.local>
2017-01-16 12:26:04 +0100

    Revert "Aggiornate le cartelle "soundtrack" e "sound effects" con tutti gli audio confermati e da confermare. Spostata la scena "Audio_Test" dalla cartella "Final_Levels" alla cartella "Test_Levels""
    This reverts commit a3bcd9c66a5a265c7eff5108dab40781e22a3711.

---
+++ Assets/Sounds/Sound Effects/Audio_Test/TestAudioScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(UnityEngine.AudioSource))]
+
+public class TestAudioScript : MonoBehaviour {
+
+    public AudioClip insertClip;
+    private AudioSource audioS;
+    private Animator charAnimator;
+
+    void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+        charAnimator = transform.root.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter (Collider col)
+    {
+        Debug.Log("Collido!!!" + transform.root);
+        
+        if (col.gameObject.tag == "Floor")
+        {
+            Debug.Log("CollidoFloor");
+            if (charAnimator.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
+            {
+                audioS.PlayOneShot(insertClip);
+            }
+        }
+
+    }
+}
