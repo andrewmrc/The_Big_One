ee162be4156f18a1325a43ad27d857df318763bb
dbga_14 <dbga_14@dbga.local>
2017-01-17 13:28:11 +0100

    Creato script per effetto sonoro durante la mira

---
+++ Assets/Sounds/AimingSound.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(UnityEngine.AudioSource))]
+
+public class AimingSound : MonoBehaviour {
+
+    private AudioSource audio;
+    public AudioClip otherClip;
+
+    // Use this for initialization
+    void Start () {
+
+        audio = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+            audio.clip = otherClip;
+            audio.Play();
+        }
+
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+            {
+            audio.clip = otherClip;
+            audio.Stop();
+        }
+    }
+}
