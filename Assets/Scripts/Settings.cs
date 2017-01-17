7ceba4858f10ce06a623d18fdf88b8720ecc8caa
David Fiorin <david94lozoo@gmail.com>
2017-01-17 18:55:14 +0100

    - Creato un Panel Settings nel quale dovrebbe essere possibile disabilitare SSAO, Bloom, Chromatic Abberation e Vignette.... Da sistemare

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityStandardAssets.Cameras;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Settings : MonoBehaviour
+{
+
+    
+}
