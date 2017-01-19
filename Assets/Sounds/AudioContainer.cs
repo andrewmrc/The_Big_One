1e826b5623382bb888e029e169b6f915880fe740
dbga_14 <dbga_14@dbga.local>
2017-01-19 16:53:00 +0100

    Creato script "AudioContainer" e modificato script "AudioManager". Ora è tutto più "pulito"

---
+++ Assets/Sounds/AudioContainer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioContainer : MonoBehaviour {
+
+    public AudioClip FatigueSound;
+    public AudioClip OpenDoorSound;
+    public AudioClip CloseDoorSound;
+    public AudioClip ElevatorFloorSound;
+    public AudioClip ControlBodyONSound;
+    public AudioClip ControlBodyOFFSound;
+    public AudioClip SwitchSound;
+    public AudioClip OfficeNoiseSound;
+    public AudioClip Footstep1;
+    public AudioClip Footstep2;
+    public AudioClip Footstep3;
+    public AudioClip Footstep4;
+    public AudioClip KeyboardTypingSound;
+    public AudioClip InsertCDSound;
+    public AudioClip AimSound;
+    public AudioClip PhoneSound;
+    public AudioClip MagneticCardSound;
+    public AudioClip CoughSound;
+
+}
--- Assets/Sounds/AudioManager.cs
+++ Assets/Sounds/AudioManager.cs
@@ -4,34 +4,40 @@ using System.Collections;
 
 public class AudioManager : MonoBehaviour
 {
-	private AudioSource m_AudioSource;
+    private AudioSource m_AudioSource;
 
-	private bool isWalking;
+    private bool isWalking;
+    private bool isAiming;
 
-	private Animator charAnimator;
+    private Animator charAnimator;
 
-	public AudioClip aimingSound;
+    public AudioClip aimingSound;
+    Coroutine footStepCo;
 
     [Space(5)]
     public AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
 
-	[Header("[Tempo tra ogni suono di passo]")]
-	public float stepTime;
+    [Header("[Tempo tra ogni suono di passo]")]
+    public float stepTime;
 
 
-	void Start () 
-	{
-		isWalking = false;
-		m_AudioSource = GetComponent<AudioSource>();
-		charAnimator = GetComponent<Animator> ();
-	}
+    void Start()
+    {
+        isWalking = false;
+        m_AudioSource = GetComponent<AudioSource>();
+        charAnimator = GetComponent<Animator>();
+    }
 
-	void FixedUpdate () 
-	{
+    void Update()
+    {
 
         if (Input.GetKeyDown(KeyCode.Mouse1) && !GameManager.Self.cantUsePower)
         {
             Debug.Log("AIMING");
+            isAiming = true;
+            StopCoroutine(footStepCo);
+            isWalking = false;
+            m_AudioSource.Stop();
             m_AudioSource.clip = aimingSound;
             m_AudioSource.Play();
         }
@@ -39,41 +45,50 @@ public class AudioManager : MonoBehaviour
         {
             m_AudioSource.clip = aimingSound;
             m_AudioSource.Stop();
+            isAiming = false;
         }
 
         if (charAnimator.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
-		{
-			if (!isWalking) {
-				Debug.Log ("WALK");
-				isWalking = true;
-				//Facendo così la clip viene fatta partire ma non seguirà nè i movimenti precisi del personaggio nè qualche tipo di variabile temporale imposta da noi.
-				//m_AudioSource.clip = otherClip;
-				//m_AudioSource.Play();
+        {
+            if (!isWalking)
+            {
+                Debug.Log("WALK");
+                isWalking = true;
+                //Facendo così la clip viene fatta partire ma non seguirà nè i movimenti precisi del personaggio nè qualche tipo di variabile temporale imposta da noi.
+                //m_AudioSource.clip = otherClip;
+                //m_AudioSource.Play();
 
-				//Questa coroutine serve se vogliamo passare noi ogni quanto tempo deve sentirsi il rumore di un singolo passo.
-				StartCoroutine (PlayFootStepSound (stepTime)); 
-			}
+                //Questa coroutine serve se vogliamo passare noi ogni quanto tempo deve sentirsi il rumore di un singolo passo.
+                footStepCo = StartCoroutine(PlayFootStepSound(stepTime));
+            }
 
-		} else if(charAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))	{
-			isWalking = false;
-			//m_AudioSource.clip = otherClip;
-			//m_AudioSource.Stop();
-		}
-	}
+        }
+        else if (charAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            isWalking = false;
+            //m_AudioSource.clip = otherClip;
+            //m_AudioSource.Stop();
+        }
+    }
 
 
-	private IEnumerator PlayFootStepSound(float waitTime) {
-		while (isWalking) {
-			yield return new WaitForSeconds (waitTime);
-			// pick & play a random footstep sound from the array,
-			// excluding sound at index 0
-			int n = Random.Range (1, m_FootstepSounds.Length);
-			m_AudioSource.clip = m_FootstepSounds [n];
-			m_AudioSource.PlayOneShot (m_AudioSource.clip);
-			// move picked sound to index 0 so it's not picked next time
-			m_FootstepSounds [n] = m_FootstepSounds [0];
-			m_FootstepSounds [0] = m_AudioSource.clip;
-		}
-	}
+    private IEnumerator PlayFootStepSound(float waitTime)
+    {
+        while (isWalking)
+        {
+            // pick & play a random footstep sound from the array,
+            // excluding sound at index 0
+            if (!isAiming)
+            {
+                int n = Random.Range(1, m_FootstepSounds.Length);
+                m_AudioSource.clip = m_FootstepSounds[n];
+                m_AudioSource.PlayOneShot(m_AudioSource.clip);
+                // move picked sound to index 0 so it's not picked next time
+                m_FootstepSounds[n] = m_FootstepSounds[0];
+                m_FootstepSounds[0] = m_AudioSource.clip;
+            }
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
 
 }
