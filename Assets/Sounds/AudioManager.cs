eeeec95afe89fc340a567533dad91e458c3b0226
dbga_14 <dbga_14@dbga.local>
2017-01-30 18:55:09 +0100

    Applicato Audio Source a tutti gli NPC. Modificato script AudioManager

--- Assets/Sounds/AudioManager.cs
+++ Assets/Sounds/AudioManager.cs
@@ -46,6 +46,7 @@ public class AudioManager : MonoBehaviour
             isWalking = false;
             m_AudioSource.Stop();
             m_AudioSource.clip = soundContainer.AimSound;
+            m_AudioSource.volume = 1;
             m_AudioSource.Play();
         }
         else if (Input.GetKeyUp(KeyCode.Mouse1))
@@ -91,6 +92,7 @@ private IEnumerator PlayFootStepSound(float waitTime)
             {
                 int n = Random.Range(1, m_FootstepSounds.Length);
                 m_AudioSource.clip = m_FootstepSounds[n];
+                m_AudioSource.volume = 0.35f;
                 m_AudioSource.PlayOneShot(m_AudioSource.clip);
                 // move picked sound to index 0 so it's not picked next time
                 m_FootstepSounds[n] = m_FootstepSounds[0];
