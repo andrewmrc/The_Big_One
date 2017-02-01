06ec67d5da3a2806be54846440f442aef525e055
dbga_35 <dbga_35@dbga.local>
2017-02-01 15:58:51 +0100

    New editor tool for U_ID

---
+++ Assets/Scripts/Editor Script/IdComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Placeholder for IdGenerator script
+public class UniqueIdentifierAttribute : PropertyAttribute { }
+
+public class IdComponent : MonoBehaviour
+{
+    [UniqueIdentifier]
+    public string uniqueId;
+   
+}
