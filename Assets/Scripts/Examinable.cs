e26a383a09342ed7a965cc36c0dbcd53212429c2
Fushja <fabrizio.caggianelli@gmail.com>
2017-02-03 00:41:21 +0100

    Work In Progress better object examinator -Attualmente testabile sulla scena TestFabri una piccola parte

---
+++ Assets/Scripts/Examinable.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Examinable : ExamineAbstract {
+
+    Coroutine ClickCO;
+    
+
+    public override void ClickMe()
+    {
+        this.transform.GetChild(0).gameObject.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log(gameObject.name);
+        }
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator DoStuff()
+    {
+        while (true)
+        {
+            Debug.Log(gameObject.name);
+            yield return null;
+        }
+        
+    }
+    public override void StopClickMe()
+    {
+        this.transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
