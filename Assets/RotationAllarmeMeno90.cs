476c96553acfbf30ec0067200daff6e68063d9ba
dbga_71 <dbga_71@dbga.local>
2017-02-03 16:23:42 +0100

    Inserite prove allarme. Schiacciand l'allarme si accendono le luci di allarme. Per ora solo nella prigione I due script servono per far girare le luci

---
+++ Assets/RotationAllarmeMeno90.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationAllarmeMeno90 : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, -90, 0) * Time.deltaTime);
+    }
+}
+
