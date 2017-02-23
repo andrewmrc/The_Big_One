c0ec3eebaa533e1e6391844cd6a076af9164a187
dbga_14 <dbga_14@dbga.local>
2017-02-23 12:11:35 +0100

    Modificato AudioManager. Sistemato audio nella scena Ufficio 1

--- Assets/Sounds/AudioManager.cs
+++ Assets/Sounds/AudioManager.cs
@@ -96,7 +96,7 @@ private IEnumerator PlayFootStepSound(float waitTime)
             {
                 int n = Random.Range(1, m_FootstepSounds.Length);
                 m_AudioSource.clip = m_FootstepSounds[n];
-                m_AudioSource.volume = 0.05f;
+                m_AudioSource.volume = 0.015f;
                 m_AudioSource.PlayOneShot(m_AudioSource.clip);
                 // move picked sound to index 0 so it's not picked next time
                 m_FootstepSounds[n] = m_FootstepSounds[0];
