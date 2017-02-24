e0b24922a461f6763da4bdd9c82d94e0e86a4dbd
dbga_04 <dbga_04@dbga.local>
2017-02-24 19:08:43 +0100

    Iniziato a modificare door handler

---
+++ Assets/Scripts/DoorItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+[Serializable]
+public class DoorItem {
+
+	public GameObject npcObject;
+	public List<string> returnMessage;
+	//public List<AudioClip> audioSpeech;
+	//public bool executed;
+	//public UnityEvent eventToActivate;
+}
