8a5bad3eb8d0d699a582c6be96c44c4476172352
Andrea Mercuri <andrea.mercuri21@gmail.com>
2017-03-06 23:26:17 +0100

    - sequenza scene aggiornata. finale finale - iniziato prove per tremolio camera

---
+++ Assets/Scripts/ShakeCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShakeCamera : MonoBehaviour {
+
+	public bool shakeTime;
+	public float shakeAmount = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shakeTime == true)
+		{
+			Camera.main.transform.root.position = Random.onUnitSphere * shakeAmount;
+		}else{
+			//Camera.main.transform.localPosition = Vector3.zero;
+		}
+	}
+
+	public void SetShakeTrue (bool value) {
+		shakeTime = value;
+	}
+
+}
