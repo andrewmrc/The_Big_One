e57ee5dc363d1ec748944ac4a8f6c63d3dd96867
Riccardo Torregrossa <Riccardo Torregrossa>
2017-03-07 00:04:05 +0100

    Regolati audio passi e mira. Regolate soundtrack

--- Assets/Sounds/AudioManager.cs
+++ Assets/Sounds/AudioManager.cs
@@ -95,7 +95,7 @@ public class AudioManager : MonoBehaviour
             {
                 int n = Random.Range(1, m_FootstepSounds.Length);
                 m_AudioSource.clip = m_FootstepSounds[n];
-                m_AudioSource.volume = 0.017f;
+                m_AudioSource.volume = 0.02f;
                 m_AudioSource.PlayOneShot(m_AudioSource.clip);
                 // move picked sound to index 0 so it's not picked next time
                 m_FootstepSounds[n] = m_FootstepSounds[0];
