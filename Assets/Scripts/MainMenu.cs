d715d772dba20047aedfe4e7ca2ea8a9ed40c7c4
Andrea Mercuri <andrea.mercuri21@gmail.com>
2017-03-06 22:41:48 +0100

    - Fixato load nel main menu - fixato due esaminabili in office

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,11 +3,13 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityStandardAssets.ImageEffects;
 
 public class MainMenu : MonoBehaviour {
 
 	Fader refFader;
 	public Button newGameButton;
+	public GameObject mainMenuCanvas;
 
 	public void Start () {
 		refFader = FindObjectOfType<Fader>();
@@ -20,6 +22,22 @@ public class MainMenu : MonoBehaviour {
 		refFader.StartCoroutine(refFader.FadeIn());
 		StartCoroutine (LoadScene (newGameLevel));
     }
+
+
+	public void LoadGameBtn()
+	{
+		refFader.GetComponent<Canvas> ().sortingOrder = 10;
+		mainMenuCanvas.transform.GetComponent<Settings> ().loadText.SetActive (true);
+		refFader.StartCoroutine(refFader.FadeIn());
+		StartCoroutine(EndLoad());
+	}
+
+
+	IEnumerator EndLoad () {
+		yield return new WaitForSeconds (3f);
+		mainMenuCanvas.transform.GetComponent<Settings> ().LoadFunction (0);
+	}
+
 
     public void ExitGameBtn()
     {
