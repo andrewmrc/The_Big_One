48c2858ed0cf8c980f33f00c9028fe8a29822d16
Riccardo Torregrossa <Riccardo Torregrossa>
2017-03-07 00:27:24 +0100

    Aggiunto suono ascensore con relativo script e mixer

---
+++ Assets/Sounds/ElevatorSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(UnityEngine.AudioSource))]
+
+public class ElevatorSound : MonoBehaviour {
+
+	private AudioSource Audio;
+	private AudioContainer soundContainer;
+
+	// Use this for initialization
+	void Start () {
+		soundContainer = GameManager.Self.GetComponent<AudioContainer>();
+		Audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Awake () {
+		Audio.clip = soundContainer.ElevatorSound;
+		Audio.volume = 0.1f;
+		Audio.PlayOneShot (Audio.clip);
+	
+	}
+}
