b30a785a269856905237888f807fd1a8a23e21e0
Andrea Mercuri <andrea.mercuri21@gmail.com>
2017-03-07 03:47:51 +0100

    - aggiornata cutscene animata - inserita cutscene 1B - aggiornati cheatcode

--- Assets/Scripts/CheatCode.cs
+++ Assets/Scripts/CheatCode.cs
@@ -20,25 +20,25 @@ public class CheatCode : MonoBehaviour {
 			StartCoroutine (LoadScene (2));
 		} else if(Input.GetKeyDown(KeyCode.Alpha3)) {
 			refFader.StartCoroutine(refFader.FadeIn());
-			StartCoroutine (LoadScene (4));
+			StartCoroutine (LoadScene (5));
 		} else if(Input.GetKeyDown(KeyCode.Alpha4)) {
 			refFader.StartCoroutine(refFader.FadeIn());
-			StartCoroutine (LoadScene (6));
+			StartCoroutine (LoadScene (7));
 		} else if(Input.GetKeyDown(KeyCode.Alpha5)) {
 			refFader.StartCoroutine(refFader.FadeIn());
-			StartCoroutine (LoadScene (7));
+			StartCoroutine (LoadScene (8));
 		} else if(Input.GetKeyDown(KeyCode.Alpha6)) {
 			refFader.StartCoroutine(refFader.FadeIn());
-			StartCoroutine (LoadScene (8));
+			StartCoroutine (LoadScene (9));
 		} else if(Input.GetKeyDown(KeyCode.Alpha7)) {
 			refFader.StartCoroutine(refFader.FadeIn());
-			StartCoroutine (LoadScene (10));
+			StartCoroutine (LoadScene (11));
 		} else if(Input.GetKeyDown(KeyCode.Alpha8)) {
 			refFader.StartCoroutine(refFader.FadeIn());
-			StartCoroutine (LoadScene (13));
+			StartCoroutine (LoadScene (14));
 		} else if(Input.GetKeyDown(KeyCode.Alpha9)) {
 			refFader.StartCoroutine(refFader.FadeIn());
-			StartCoroutine (LoadScene (14));
+			StartCoroutine (LoadScene (15));
 		} else if(Input.GetKeyDown(KeyCode.Alpha0)) {
 			refFader.StartCoroutine(refFader.FadeIn());
 			StartCoroutine (LoadScene (SceneManager.GetActiveScene().buildIndex));
